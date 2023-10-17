20913bf12d5c08991b13a6d983481de3cebf62ef
jasowantadas <jasowanta21@gmail.com>
2023-10-17 14:48:59 +0530

    Introduction to ejs

--- nodejs_practice_1/index.js
+++ nodejs_practice_1/index.js
@@ -18,7 +18,12 @@ const app = express();
 app.use(express.static(path.join(path.resolve(), "public"))); //To access values directly from public folder as static values
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile("index.html"); //Because of express.static we now dont have to set any abolute path to the function
+});
+app.get("/form", (req, res) => {
+  const ggwp = 10,
+    g = 1;
+  res.render("index.ejs", { ggwp, g });
 });
 
 app.listen(5000, () => {
